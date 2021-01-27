f9a4b5745f00fbbdd169498c419e378f02fbbf3a
unknown <shahrukh1494@gmail.com>
2021-01-27 16:49:04 +0530

    hide show buttons added

--- javascript.js
+++ javascript.js
@@ -2,3 +2,11 @@ function clickevent()
 {
     document.getElementById("demo").innerHTML = "See it works!";
 }
+function hidetext()
+{
+    document.getElementById("demo").style.display = "none";
+}
+function showtext()
+{
+    document.getElementById("demo").style.display = "block";
+}
