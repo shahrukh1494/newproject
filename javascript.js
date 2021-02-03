fb050c980c039efb44a677bce6ba8d02b2b87e6e
unknown <shahrukh1494@gmail.com>
2021-02-03 14:03:43 +0530

    implemented cleaner looking CSS and removed precision for numbers in JS

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ let additionOfNumbers = () => {
     let first, sec, sum;
     first = document.getElementById("firstnum").value;
     sec = document.getElementById("secnum").value;
-    sum = Number(first, 10) + Number(sec, 10);
+    sum = Number(first) + Number(sec);
     document.getElementById("add").innerHTML = "The sum is " + sum;
     document.getElementById("addAgain").innerHTML = `The sum again is ${sum} (This time using template literals)`;
 }
