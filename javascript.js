adb3a48ebb296f669e96e1d82e964f34e6ec0ccd
unknown <shahrukh1494@gmail.com>
2021-02-08 17:29:41 +0530

    added date and time

--- javascript.js
+++ javascript.js
@@ -26,3 +26,39 @@ let additionOfNumbers = () => {
     document.getElementById("addAgain").innerHTML = `The sum again is ${sum} (This time using template literals)`;
 }
 let add = additionOfNumbers;
+
+//fuction to display current day of the month and date
+let dispDate = () => {
+    let n =  new Date();
+    let y = n.getFullYear();
+    let m = n.getMonth() + 1;
+    let d = n.getDate();
+    let dy = n.getDay();
+    let dname;
+    if (dy === 0) {
+        dname = "Sunday";
+    }
+    else if (dy === 1) {
+        dname = "Monday";
+    }
+    else if (dy === 2) {
+        dname = "Tuesday";
+    }
+    else if (dy === 3) {
+        dname = "Wednesday";
+    }
+    else if (dy === 4) {
+        dname = "Thursday";
+    }
+    else if (dy === 5) {
+        dname = "Friday";
+    }
+    else if (dy === 6) {
+        dname = "Saturday";
+    }
+    else {
+        dname = "unknown";
+    }
+    console.log(dname);
+    document.getElementById("date").innerHTML = `Today is ${dname}, ${d} / ${m} / ${y}`;
+}
